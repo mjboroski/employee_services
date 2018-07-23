ffb3206d44aea134edd0ca6594aabc7299af07bf
mjboroski <mjboroski@gmail.com>
2018-07-23 14:55:46 -0400

    got user show page functional, after conflict

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -55,28 +55,4 @@ $(function(){
   }
 
   routeMaker()
-
-  $("form.edit_user").hide()
-  $('div.actions input').removeAttr('data-disable-with');
-
-  $("p.edit_account a").on("click", function(e){
-    $("form.edit_user").toggle("fast");
-    e.preventDefault();
-  })
-
-  $("form.edit_user").on("submit", function(e){
-    var url = this.action
-    var data = $(this).serialize();
-    $.ajax({
-      type: "patch",
-      url: url,
-      data: data,
-      success: function(response){
-        $("form.edit_user").hide("fast");
-        $(".greeting").contents().remove();
-        $(".greeting").append(response);
-      }
-    })
-    e.preventDefault();
-  })
 })
